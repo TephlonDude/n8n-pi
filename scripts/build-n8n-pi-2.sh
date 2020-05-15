8921402b93fb980f455468ab6f546faaf3979918
TephlonDude <jason@mcfeetors.com>
2020-05-14 22:33:09 -0600

    Enabled logging

--- scripts/build-n8n-pi-2.sh
+++ scripts/build-n8n-pi-2.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
-echo "Script starting. Please wait..."
+logfile=/var/log/n8n-pi.log
 
-PROGNAME=$(basename $0)
-
+# Deals with errors
 error_exit()
 {
-	echo "${PROGNAME}: ${1:-"Unknown Error"}" 1>&2
+	echo "${1:-"Unknown Error"}" 1>&2
+    echo "Last 10 entried by this script:"
+    tail $logfile
+    echo "Full log details are recorded in $logfile"
+    echo
     echo "You can rerun this script by using the command:"
     echo "wget -O - https://raw.githubusercontent.com/TephlonDude/n8n-pi/master/scripts/build-n8n-pi-1.sh | bash"
 	exit 1
 }
 
+# Create log headings
+log_heading()
+{
+    length=${#1}
+    length=`expr length + 8`
+    printf '%*s' $length | tr ' ' '*'>>$logfile
+    echo>>$logfile
+    echo "*** $1 ***">>$logfile
+    printf '%*s' $length | tr ' ' '*'>>$logfile
+    echo>>$logfile
+    echo -n $1
+
+}
+
 elevate()
 {
     if [[ $EUID -ne 0 ]]; then
-        echo $sudo_pwd | sudo -S $1
+        echo $sudo_pwd | sudo -S $1 &>>$logfile
     else
-        echo $1
+        echo $1 &>>$logfile
     fi
     return $?
 }
 
-
+clear
 
 # Checking for internet access
-ping -c 3 raw.githubusercontent.com >/dev/null 2>&1 || error_exit "$LINENO: Unable to access the internet. Script exiting"
+log_heading "Checking for internet access..."
+ping -c 5 raw.githubusercontent.com &>>$logfile || error_exit "$LINENO: Unable to access the internet. Script exiting"
+echo "done!"
 
 message='This script is designed to build a new n8n-pi from a base Raspbian Lite installation.\n\nThis is the second of two scripts that need to be run.\n\nIt will perform the following actions:\n    1. Clean up from previous script\n    2. Delete the pi user\n    3. Install NodeJS\n    4. Install n8n\n    5. Install & Configure PM2\n    6. Update MOTD\n    7. Reboot'
 whiptail --backtitle "n8n-pi Installer" --title "n8n-pi Installer Part 2" --msgbox "$message"  18 78
@@ -41,11 +60,10 @@ if (whiptail --backtitle "n8n-pi Installer" --title "Continue with install?" --y
     fi
 
     # Clean up the temporary changes to .bashrc that allowed this script to autorun
-    message=$'\U2192 Previous script cleanup\n  Delete pi user\n  Install NodeJS\n  Install n8n\n  Install & Configure PM2\n  Update MOTD\n  Reboot'
-    whiptail --backtitle "n8n-pi Installer" --gauge "$message" 11 34 0
-    mv ~/.bashrc ~/.bashrc-temp || error_exit "$LINENO: Unable to rename temp .bashrc file"
-    mv ~/.bashrc-org ~/.bashrc || error_exit "$LINENO: Unable to rename original .bashrc file"
-    rm -f ~/.bashrc-temp || error_exit "$LINENO: Unable to delete temp .bashrc file"
+    log_heading "Clean up the temporary changes to .bashrc that allowed this script to autorun..."
+    mv ~/.bashrc ~/.bashrc-temp &>>$logfile || error_exit "$LINENO: Unable to rename temp .bashrc file"
+    mv ~/.bashrc-org ~/.bashrc &>>$logfile || error_exit "$LINENO: Unable to rename original .bashrc file"
+    rm -f ~/.bashrc-temp &>>$logfile || error_exit "$LINENO: Unable to delete temp .bashrc file"
 
     # Delete the pi user and remove the home folder
     message=$'  Previous script cleanup\n\U2192 Delete pi user\n  Install NodeJS\n  Install n8n\n  Install & Configure PM2\n  Update MOTD\n  Reboot'
@@ -64,29 +82,29 @@ if (whiptail --backtitle "n8n-pi Installer" --title "Continue with install?" --y
     # DEBURL="https://deb.nodesource.com/setup_${NODEVER}"
     message=$'  Previous script cleanup\n  Delete pi user\n\U2192  Install NodeJS\n  Install n8n\n  Install & Configure PM2\n  Update MOTD\n  Reboot'
     whiptail --backtitle "n8n-pi Installer" --gauge "$message" 11 34 33
-    curl -sL https://deb.nodesource.com/setup_${NODEVER} | sudo -E bash - || error_exit "$LINENO: Unable to update NodeJs source list"
+    curl -sL https://deb.nodesource.com/setup_${NODEVER} | sudo -E bash - &>>$logfile || error_exit "$LINENO: Unable to update NodeJs source list"
     elevate "apt install -y nodejs" || error_exit "$LINENO: Unable to install NodeJS"
 
     # Install n8n
     message=$'  Previous script cleanup\n  Delete pi user\n   Install NodeJS\n\U2192 Install n8n\n  Install & Configure PM2\n  Update MOTD\n  Reboot'
     whiptail --backtitle "n8n-pi Installer" --gauge "$message" 11 34 50
-    cd ~ || error_exit "$LINENO: Unable to change working directory to home directory"
+    cd ~ &>>$logfile || error_exit "$LINENO: Unable to change working directory to home directory"
     # elevate "chown -R n8n:n8n /usr/lib/node_modules || error_exit "$LINENO: Unable to change ownership of the /usr/lib/node_modules folder to user n8n"
-    mkdir ~/.nodejs_global || error_exit "$LINENO: Unable to create ~/.nodejs_global"
-    npm config set prefix ~/.nodejs_global || error_exit "$LINENO: Unable to set the npm prefix to ~/.nodejs_global"
-    echo 'export PATH=~/.nodejs_global/bin:$PATH' | tee --append ~/.profile >/dev/null || error_exit "$LINENO: Unable to update ~/.profile to update PATH variable"
-    source ~/.profile || error_exit "$LINENO: Unable to reload ~/.profile "
-    npm install n8n -g || error_exit "$LINENO: Unable to install n8n"
+    mkdir ~/.nodejs_global &>>$logfile || error_exit "$LINENO: Unable to create ~/.nodejs_global"
+    npm config set prefix ~/.nodejs_global &>>$logfile || error_exit "$LINENO: Unable to set the npm prefix to ~/.nodejs_global"
+    echo 'export PATH=~/.nodejs_global/bin:$PATH' | tee --append ~/.profile &>>$logfile || error_exit "$LINENO: Unable to update ~/.profile to update PATH variable"
+    source ~/.profile &>>$logfile || error_exit "$LINENO: Unable to reload ~/.profile "
+    npm install n8n -g &>>$logfile || error_exit "$LINENO: Unable to install n8n"
 
     # Install & Configure PM2
     message=$'  Previous script cleanup\n  Delete pi user\n   Install NodeJS\n  Install n8n\n\U2192 Install & Configure PM2\n  Update MOTD\n  Reboot'
     whiptail --backtitle "n8n-pi Installer" --gauge "$message" 11 34 67
-    cd ~ || error_exit "$LINENO: Unable to move the the home directory"
-    npm install pm2@latest -g || error_exit "$LINENO: Unable to install PM2"
+    cd ~ &>>$logfile || error_exit "$LINENO: Unable to move the the home directory"
+    npm install pm2@latest -g &>>$logfile || error_exit "$LINENO: Unable to install PM2"
     if (whiptail  --backtitle "n8n-pi Installer" --title "Tunnel?" --yesno "Do you wish to start n8n with the tunnel option?" 8 40); then
-        pm2 start n8n --tunnel || error_exit "$LINENO: Unable to start n8n with tunnel option using PM2"
+        pm2 start n8n --tunnel &>>$logfile || error_exit "$LINENO: Unable to start n8n with tunnel option using PM2"
     else
-        pm2 start n8n || error_exit "$LINENO: Unable to start n8n using PM2"
+        pm2 start n8n &>>$logfile || error_exit "$LINENO: Unable to start n8n using PM2"
     fi
 
     # Install Updated MOTD
@@ -100,7 +118,6 @@ if (whiptail --backtitle "n8n-pi Installer" --title "Continue with install?" --y
     message=$'The final phase of the installation has finished. We must now reboot the system in order for some changes to take effect.\n\nWhen the system comes back online, you should be able to access the n8n WebUI from https://${IPADDR}:5678.'
     whiptail --backtitle "n8n-pi Installer" --title "Time to Reboot" --msgbox "$message"  17 78
     elevate "reboot" || error_exit "$LINENO: Unable to reboot"
-
 
 else 
     error_exit "$LINENO: Installation cancelled"
